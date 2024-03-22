c453991ed54177cca77189964d3da934391c84e8
Nikhl <nikhil.singh@xenonstack.com>
2024-03-22 12:30:49 +0530

    changes

--- backend/main.go
+++ backend/main.go
@@ -16,17 +16,17 @@ func main() {
 
 	//admin roles
 
-	r.POST("/libraries", handlers.CreateLibrary)             //library creation
-	r.POST("/add-book", handlers.AddBook)                    //addition of books
-	r.PATCH("/update-book", handlers.UpdateBook)             //updation of books
-	r.DELETE("/remove-book", handlers.RemoveBook)            //deletion of books
-	r.GET("/list-issue-request", handlers.ListIssueRequests) //list of issues request
+	r.POST("/libraries", handlers.CreateLibrary)                                //library creation
+	r.POST("/add-book", handlers.AddBook)                                       //addition of books
+	r.PATCH("/update-book", handlers.UpdateBook)                                //updation of books
+	r.DELETE("/remove-book", handlers.RemoveBook)                               //deletion of books
+	r.GET("/list-issue-request", handlers.ListIssueRequests)                    //list of issues request
+	r.POST("/approve-issue-request/:request_id", handlers.ApprovedIssueRequest) //approve request
+	r.DELETE("/reject-issue-request/:request_id", handlers.RejectIssueRequest)  //reject request
 
 	//readers role
-	r.POST("/search/book", handlers.SearchBook)
-	r.POST("/issue/request", handlers.RaiseIssueRequest)
-	r.POST("/approve-issue-request/:request_id", handlers.ApprovedIssueRequest)
-	r.DELETE("/reject-issue-request/:request_id", handlers.RejectIssueRequest)
+	r.POST("/search/book", handlers.SearchBook)          //search book
+	r.POST("/issue/request", handlers.RaiseIssueRequest) //raise request
 
 	r.Run(":8088") //running on port localhost:8088
 
